96587fd1d98b61731e457f3e7ecebb40e56c9a8d
ToniA <ToniA@users.noreply.github.com>
2017-04-02 11:30:37 +0300

    Config in Unicode

--- config_lomamaja2.lua
+++ config_lomamaja2.lua
@@ -2,11 +2,11 @@
 
 apartment_configs = {
   {apartment = 'A', rooms = {{name = 'alakerta', delayed = 1},
-                             {name = 'yl‰kerta', delayed = 0}}},
+                             {name = 'yl√§kerta', delayed = 0}}},
   {apartment = 'B', rooms = {{name = 'alakerta', delayed = 1},
-                             {name = 'yl‰kerta', delayed = 0}}},
+                             {name = 'yl√§kerta', delayed = 0}}},
   {apartment = 'C', rooms = {{name = 'alakerta', delayed = 1},
-                             {name = 'yl‰kerta', delayed = 0}}},
+                             {name = 'yl√§kerta', delayed = 0}}},
   {apartment = 'D', rooms = {{name = 'alakerta', delayed = 1},
-                             {name = 'yl‰kerta', delayed = 0}}}
+                             {name = 'yl√§kerta', delayed = 0}}}
 }
