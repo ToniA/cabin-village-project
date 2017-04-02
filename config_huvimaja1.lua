04cebdad9e5d97419c095fc8545464eed98cef81
ToniA <ToniA@users.noreply.github.com>
2017-04-02 10:51:02 +0300

    Configuration for huvimaja1 and lomamaja2

---
+++ config_huvimaja1.lua
@@ -0,0 +1,12 @@
+-- Apartments configuration
+
+apartment_configs = {
+  {apartment = 'A', rooms = {{name = 'alakerta', delayed = 1},
+                             {name = 'yläkerta', delayed = 0}}}, 
+  {apartment = 'B', rooms = {{name = 'alakerta', delayed = 1}, 
+                             {name = 'yläkerta', delayed = 0}}},
+  {apartment = 'C', rooms = {{name = 'alakerta', delayed = 1}, 
+                             {name = 'yläkerta', delayed = 0}}},
+  {apartment = 'D', rooms = {{name = 'alakerta', delayed = 1}, 
+                             {name = 'yläkerta', delayed = 0}}}
+}
