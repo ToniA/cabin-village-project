389c83c4b1cfbb768109858bf4696a2db8b20746
ToniA <ToniA@users.noreply.github.com>
2017-04-02 11:34:37 +0300

    Config in Unicode

--- config_huvikumpu12.lua
+++ config_huvikumpu12.lua
@@ -2,13 +2,13 @@
 
 apartment_configs = {
   {apartment = 'A', rooms = {{name = 'alakerta', delayed = 1},
-                             {name = 'yl‰kerta 1', delayed = 0},
-                             {name = 'yl‰kerta 2', delayed = 0}}},
+                             {name = 'yl√§kerta 1', delayed = 0},
+                             {name = 'yl√§kerta 2', delayed = 0}}},
   {apartment = 'B', rooms = {{name = 'alakerta', delayed = 1},
-                             {name = 'yl‰kerta', delayed = 0}}},
+                             {name = 'yl√§kerta', delayed = 0}}},
   {apartment = 'C', rooms = {{name = 'alakerta', delayed = 1},
-                             {name = 'yl‰kerta', delayed = 0}}},
+                             {name = 'yl√§kerta', delayed = 0}}},
   {apartment = 'D', rooms = {{name = 'alakerta', delayed = 1},
-                             {name = 'yl‰kerta 1', delayed = 0},
-                             {name = 'yl‰kerta 2', delayed = 0}}}
+                             {name = 'yl√§kerta 1', delayed = 0},
+                             {name = 'yl√§kerta 2', delayed = 0}}}
 }
--- config_huvimaja1.lua
+++ config_huvimaja1.lua
@@ -2,11 +2,11 @@
 
 apartment_configs = {
   {apartment = 'A', rooms = {{name = 'alakerta', delayed = 1},
-                             {name = 'yläkerta', delayed = 0}}}, 
-  {apartment = 'B', rooms = {{name = 'alakerta', delayed = 1}, 
-                             {name = 'yläkerta', delayed = 0}}},
-  {apartment = 'C', rooms = {{name = 'alakerta', delayed = 1}, 
-                             {name = 'yläkerta', delayed = 0}}},
-  {apartment = 'D', rooms = {{name = 'alakerta', delayed = 1}, 
-                             {name = 'yläkerta', delayed = 0}}}
+                             {name = 'ylÃ¤kerta', delayed = 0}}},
+  {apartment = 'B', rooms = {{name = 'alakerta', delayed = 1},
+                             {name = 'ylÃ¤kerta', delayed = 0}}},
+  {apartment = 'C', rooms = {{name = 'alakerta', delayed = 1},
+                             {name = 'ylÃ¤kerta', delayed = 0}}},
+  {apartment = 'D', rooms = {{name = 'alakerta', delayed = 1},
+                             {name = 'ylÃ¤kerta', delayed = 0}}}
 }
