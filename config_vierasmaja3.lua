38f09ce38e7d31a956eb3f3d76ae2fc57cd34b03
ToniA <ToniA@users.noreply.github.com>
2017-04-02 11:51:52 +0300

    Config in Unicode

--- config_vierasmaja3.lua
+++ config_vierasmaja3.lua
@@ -3,10 +3,10 @@
 apartment_configs = {
   {apartment = 'A1', rooms = {{name = 'alakerta 1 ja 2', delayed = 1},
                               {name = 'alakerta 3', delayed = 0},
-                              {name = 'yl‰kerta 1', delayed = 0},
-                              {name = 'yl‰kerta 2', delayed = 0}}},
+                              {name = 'yl√§kerta 1', delayed = 0},
+                              {name = 'yl√§kerta 2', delayed = 0}}},
   {apartment = 'A2', rooms = {{name = 'alakerta 1 ja 2', delayed = 1},
                               {name = 'alakerta 3', delayed = 0},
-                              {name = 'yl‰kerta 1', delayed = 0},
-                              {name = 'yl‰kerta 2', delayed = 0}}}
+                              {name = 'yl√§kerta 1', delayed = 0},
+                              {name = 'yl√§kerta 2', delayed = 0}}}
 }
