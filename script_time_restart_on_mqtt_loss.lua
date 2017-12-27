fd7e7d6973b72cc0e9f9fcf005272548f592d356
ToniA <ToniA@users.noreply.github.com>
2017-12-27 16:41:47 +0200

    Reboot if no temperature data (loss of MQTT?)

---
+++ script_time_restart_on_mqtt_loss.lua
@@ -0,0 +1,46 @@
+local scriptPath = debug.getinfo(1).source:match("@?(.*/)")
+package.path = package.path .. ';' .. scriptPath .. '?.lua'
+domoticz_functions = require('domoticz_functions')
+require('settings')
+
+
+commandArray = {}
+
+restart = 1
+
+
+n = os.tmpname()
+os.execute ("var=$(ps -eo pid,args |grep 'domoticz -daemon' | grep -v grep |awk '{print $1}') && echo $(($(cut -d '.' -f1 /proc/uptime) - $(($(cut -d ' ' -f22 /proc/$var/stat)/100)))) > " .. n)
+for line in io.lines(n) do
+  UptimeInSeconds = line
+end
+os.remove(n)
+
+UptimeInSeconds = tonumber(UptimeInSeconds);
+-- print('Domoticz has been running for ' .. UptimeInSeconds .. ' seconds')
+if UptimeInSeconds < 600 then
+  restart = 0
+end
+
+
+for i, apartment_config in pairs(apartment_configs) do
+  apartment = apartment_config["apartment"]
+  for j, room_config in ipairs(apartment_config["rooms"]) do
+    lastUpdate = otherdevices_lastupdate[apartment .. ': ' .. temperatureDev .. room_config["name"]]
+    lastUpdateSeconds = domoticz_functions.timedifference(lastUpdate)
+
+    -- print('device: ' .. apartment .. ': ' .. temperatureDev .. room_config["name"])
+    -- print('last updated: ' .. lastUpdateSeconds)
+    
+    if lastUpdateSeconds < 120 then
+      restart = 0
+    end
+  end
+end
+
+if restart == 1 then
+  print('Domoticz needs to be restarted, MQTT might be down')
+  os.execute("/sbin/shutdown -r now")
+end
+
+return commandArray
