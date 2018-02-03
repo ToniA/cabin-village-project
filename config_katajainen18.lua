ae991f0dd81b240925a65418e8eb6e03ca6d2ad3
ToniA <ToniA@users.noreply.github.com>
2018-02-03 16:40:22 +0200

    Katajainen config

---
+++ config_katajainen18.lua
@@ -0,0 +1,11 @@
+-- Apartments configuration
+
+apartment_configs = {
+  {apartment = 'C', rooms = {{name = 'alakerta', delayed = 1},
+                             {name = 'yläkerta 1', delayed = 0},
+                             {name = 'yläkerta 2', delayed = 0}}},
+  {apartment = 'D', rooms = {{name = 'alakerta', delayed = 1},
+                             {name = 'yläkerta 1', delayed = 0}}},
+  {apartment = 'E', rooms = {{name = 'alakerta', delayed = 1}, 
+                             {name = 'yläkerta 1', delayed = 0}}}
+}
