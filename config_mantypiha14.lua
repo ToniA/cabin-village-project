68c6937af93767ae12efcdf2e3a0804d1e359d82
Toni <ToniA@users.noreply.github.com>
2019-07-24 15:16:18 +0300

    Mäntypiha 14 config

---
+++ config_mantypiha14.lua
@@ -0,0 +1,8 @@
+-- Apartments configuration
+
+apartment_configs = {
+  {apartment = 'A', rooms = {{name = 'alakerta', delayed = 1, powerfulTrigger = 1},
+                             {name = 'yläkerta', delayed = 0}}},
+  {apartment = 'B', rooms = {{name = 'alakerta', delayed = 1, powerfulTrigger = 1},
+                             {name = 'yläkerta', delayed = 0}}}
+}
